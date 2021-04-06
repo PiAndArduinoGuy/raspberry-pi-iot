0afd34b1611826d6f1df71e18b2f4176450022f8
Quintin Kruger <qkruger@jhb.dvt.co.za>
2021-04-06 21:08:13 +0200

    Clean up, added accomodation of code update to read and write to pump config file stored on RaspberryPi, added docker commands to determine wehther the required container is running and/or exists

--- IoT UI/control-hub/src/main/java/quintin/raspberrypi/control_hub/service/PumpConfigService.java
+++ IoT UI/control-hub/src/main/java/quintin/raspberrypi/control_hub/service/PumpConfigService.java
@@ -1,20 +1,17 @@
 package quintin.raspberrypi.control_hub.service;
 
-import java.io.IOException;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.http.HttpStatus;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
-import org.springframework.web.client.RestTemplate;
-import quintin.raspberrypi.control_hub.OverrideStatus;
 import quintin.raspberrypi.control_hub.PumpConfig;
 import quintin.raspberrypi.control_hub.exception.RaspberryPiControlHubException;
+
+import java.io.IOException;
 
 @Service
 @EnableBinding(Source.class)
