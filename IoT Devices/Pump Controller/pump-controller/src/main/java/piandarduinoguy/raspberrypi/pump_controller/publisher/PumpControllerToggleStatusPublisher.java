b4a5eaf26c682be25b563c2edde986b615752f30
Quintin Kruger <qkruger@jhb.dvt.co.za>
2021-05-04 20:10:21 +0200

    Added logging for subscribers and publishers, updated the zip jar files with updated code for Ansible deploys, removed forgotten stail code from tests

--- IoT Devices/Pump Controller/pump-controller/src/main/java/piandarduinoguy/raspberrypi/pump_controller/publisher/PumpControllerToggleStatusPublisher.java
+++ IoT Devices/Pump Controller/pump-controller/src/main/java/piandarduinoguy/raspberrypi/pump_controller/publisher/PumpControllerToggleStatusPublisher.java
@@ -1,5 +1,6 @@
 package piandarduinoguy.raspberrypi.pump_controller.publisher;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.support.MessageBuilder;
@@ -10,11 +11,13 @@ import piandarduinoguy.raspberrypi.pump_controller.domain.PumpState;
 
 @Component
 @EnableBinding(PumpControllerChannels.class)
+@Slf4j
 public class PumpControllerToggleStatusPublisher {
     @Autowired
     private PumpControllerChannels pumpControllerChannels;
 
     public void publishUpdate(PumpState pumpState) {
+        log.info("Sending message {} on the queue {}", pumpState.toString(), pumpControllerChannels.pumpControllerStateOutput());
         pumpControllerChannels.pumpControllerStateOutput().send(MessageBuilder.withPayload(pumpState.toString()).build());
     }
 }
